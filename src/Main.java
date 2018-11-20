71ace6dd30eb794d121f753483ea97c1978732c1
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-11-20 11:27:35 -0600

    need to redo exercise

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
+    @SuppressWarnings("Deprecated") //would do this for easy fix//
     public static void main(String[] args) {
         Author author = new Author("Sandi", "Metz");
 
         author.addBook("Practical Object-Oriented Design in Ruby");
         author.addBook("99 Bottles of OOP");
 
-        for (String book: author.publishedBooks()) {
+        for (String book: author.getBooks()) { //changed from getbooks but can just change and supressedwarning de//
             System.out.println(book);
         }
     }
